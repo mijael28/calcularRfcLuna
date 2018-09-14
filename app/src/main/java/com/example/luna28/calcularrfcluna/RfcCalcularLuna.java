26188d85714c76d217a9d8c1ff2659a8a5bed567
mijael28 <mijael.de.luna21@gmail.com>
2018-09-14 16:03:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/luna28/calcularrfcluna/RfcCalcularLuna.java
@@ -0,0 +1,13 @@
+package com.example.luna28.calcularrfcluna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RfcCalcularLuna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rfc_calcular_luna);
+    }
+}
